fea122188883e853cb1cc5f2dbbda480a2981788
nikolai <rochko385@gmail.com>
2026-02-24 15:07:05 +0300

    Transactions methods adding

---
+++ FinanceTracker/Transaction.h
@@ -0,0 +1,76 @@
+#pragma once
+#include<string>
+#include<iostream>
+
+class Transaction
+{
+public:
+
+	enum class Type
+	{
+		INCOME,
+		EXPENCE
+	};
+
+	enum class ExpenceCategory
+	{
+		FOOD,
+		CLOSES,
+		HOME
+	};
+
+	enum class IncomeCategory
+	{
+		SALARY,
+		CASHBACK,
+		TRANSFER
+	};
+
+	struct Time
+	{
+		int seconds;
+		int minutes;
+		int hours;
+	};
+
+	struct Date
+	{
+		int day;
+		int month;
+		int year;
+		Time time;
+	};
+
+
+	double amount;
+	Type type;
+	//ExpenceCategory expence;
+//	IncomeCategory income;
+	std::string tag;
+	Date date;
+
+private:
+
+	//Получение значения суммы double
+	double GetAmount();
+
+	//Получение знаяения типа транзакции Transaction::Type
+	Type GetType();
+
+	//Получение строки описания 
+	std::string GetTag();
+
+	//Получение структуры даты Transaction::Date
+	Date GetDate();
+
+	//Вывод типа в консоль
+	void ShowType();
+
+	//Вывод даты в консоль
+	void ShowDate();
+
+	//Вывод информации о транзакции в консоль
+	void Show();
+	
+};
+
